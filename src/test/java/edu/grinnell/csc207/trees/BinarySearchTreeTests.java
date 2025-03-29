54a70a17c8cbe95afe60627e438b7c8e2b65aaf1
CoBens14 <154711139+CoBens14@users.noreply.github.com>
2025-03-29 11:12:13 -0500

    Add files via upload

--- src/test/java/edu/grinnell/csc207/trees/BinarySearchTreeTests.java
+++ src/test/java/edu/grinnell/csc207/trees/BinarySearchTreeTests.java
@@ -19,6 +19,18 @@ public class BinarySearchTreeTests {
         return tree;
     }
 
+    private BinarySearchTree<Integer> makeSampleTree2() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
+        tree.insert(5);
+        tree.insert(0);
+        tree.insert(7);
+        tree.insert(11);
+        tree.insert(6);
+        tree.insert(-1);
+        tree.insert(2);
+        return tree;
+    }
+
     @Test
     public void emptyTreeTest() {
         BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
@@ -71,5 +83,35 @@ public class BinarySearchTreeTests {
         tree.delete(6);
         assertEquals(4, tree.size());
         assertFalse(tree.contains(6));
+    }
+
+    @Test
+    public void basicDeleteTest2() {
+        BinarySearchTree<Integer> tree = makeSampleTree();
+        assertEquals(5, tree.size());
+        assertTrue(tree.contains(7));
+        tree.delete(7);
+        assertEquals(4, tree.size());
+        assertFalse(tree.contains(7));
+    }
+
+    @Test
+    public void basicDeleteTest3() {
+        BinarySearchTree<Integer> tree = makeSampleTree2();
+        assertEquals(7, tree.size());
+        assertTrue(tree.contains(6));
+        tree.delete(0);
+        assertEquals(6, tree.size());
+        assertFalse(tree.contains(0));
+    }
+
+    @Test
+    public void basicDeleteRootTest() {
+        BinarySearchTree<Integer> tree = makeSampleTree2();
+        assertEquals(7, tree.size());
+        assertTrue(tree.contains(6));
+        tree.delete(5);
+        assertEquals(6, tree.size());
+        assertFalse(tree.contains(5));
     }
 }
